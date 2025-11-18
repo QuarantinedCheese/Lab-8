dcb6b7d6b599f99f8533533f52e084e2f035a5b4
Quarantined Cheese <ethanpothier2@gmail.com>
2025-11-18 15:25:21 -0500

    part one object

---
+++ script.js
@@ -0,0 +1,16 @@
+// PART ONE: UNDERSTANDING AND CREATING OBJECTS
+
+let student = {
+    name: "John Student",
+    age: 34,
+    enrolled: true,
+    courses: ["CPAN-133", "CPAN-113", "CPAN-134", "CPAN-111", "CPAN-116"],
+    display: function() {
+        console.log(`
+                Student name: ${this.name}\n
+                 Student age: ${this.age}\n
+            Student enrolled: ${this.enrolled}\n
+             Student courses: ${this.courses}
+            `)
+    }
+}
