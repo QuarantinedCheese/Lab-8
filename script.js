2048e997020aad992ed0c818a6289cacf274384d
Quarantined Cheese <ethanpothier2@gmail.com>
2025-11-18 15:47:09 -0500

    part 5 init

--- script.js
+++ script.js
@@ -58,3 +58,22 @@ const newCourses = ["WRIT-120", "WRIT-220"];
 
 student.courses = [...student.courses, ...newCourses];
 console.log(student.courses);
+
+
+//PART 5: OBJECT METHODS
+//I broke my naming convention! I'm evil.
+
+//Student object copied from above...
+let student = {
+    name: "John Student",
+    age: 34,
+    enrolled: true,
+    courses: ["CPAN-133", "CPAN-113", "CPAN-134", "CPAN-111", "CPAN-116"],
+    display: function() {
+        console.log(`    Student name: ${this.name}
+                 Student age: ${this.age}
+            Student enrolled: ${this.enrolled}
+             Student courses: ${this.courses}
+            `);
+    }
+};
